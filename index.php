fbfb9cd04a0d01c8b766d5cc26e1614e1398cd8f
phinehas b alabi <richie>
2016-09-29 14:29:22 -0400

    php code

--- index.php
+++ index.php
@@ -12,12 +12,36 @@ echo "$myvar";
 
 //this is an example of php arrays
 $myarray = array();
-$myarray() = 'some value 1';
-$myarray() = 'some value 2';
-$myarray() = 'some value 3';
+$myarray[] = 'some value 1';
+$myarray[] = 'some value 2';
+$myarray[] = 'some value 3';
 
 print_r($myarray);
 //this is an example of an associative array and a nested array
 $myAssoc = array('value1' => $myarray,  'value2' => $myarray);
 print_r($myAssoc);
+//this is how you print r access array by naming the key y9ou want to access
+print_r{$myAssoc[ 'value1']};
+//this is how you define a class in php
+class myclass {
+//three different property with scopes
+  public $myPublic;
+  private $myPrivate;
+  protected $myProtected;
+
+  public function __construct {} {
+  // this is the internal the arrow point to a property for method to 
+    $this ->myPublic =1;
+    $this ->myPrivate =2;
+    $this ->myProtected =3;
+    //this is how you call amethod inside the object and pass a variable or value
+    
+    public function sayHello($name) {
+    	echo 'Helo' . $name . "<br>";
+
+    }
+    }
+//this is how you instantiate
+?$obj=new myclass;
+print_r{$obj};
 ?>
