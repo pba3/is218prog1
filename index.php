3813c54646187f3b5ad2c99fa9a7a34cb6e42c9f
phinehas b alabi <richie>
2016-09-29 13:37:45 -0400

    add php code

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+//this is how you print text in php
 echo'hello world';
+//this is how you store a vale in a variable
+$myvar = 'some text i want to store in a variable';
+//this is an example of the difference between single quotes and double quotes 
+
+echo '<br>';
+echo '$myvar';
+echo '$<br>';
+echo "$myvar";
+
 ?>
